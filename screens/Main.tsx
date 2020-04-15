8225fc164267400ce5c842cab8b9f0e29758f670
TMJ97 <s185027@student.dtu.dk>
2020-04-15 12:35:54 +0200

    Main Screen

--- screens/Main.tsx
+++ screens/Main.tsx
@@ -1,8 +1,49 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import {
+	View,
+	ImageBackground,
+	StyleSheet,
+	Image,
+	TouchableOpacity,
+} from 'react-native';
 
 export default () => (
-	<View>
-		<Text>Test</Text>
+	<View style={styles.container}>
+		<ImageBackground
+			source={require('../assets/placeholderMainScreen.jpg')}
+			style={styles.backgroundImage}
+		>
+			<TouchableOpacity
+				onPress={() =>
+					console.log('Did Work Amaze Camera Button Yes Timmy')
+				}
+			>
+				<Image
+					source={require('../assets/cameraButton.png')}
+					style={styles.cameraImage}
+				></Image>
+			</TouchableOpacity>
+		</ImageBackground>
 	</View>
 );
+
+const styles = StyleSheet.create({
+	container: {
+		justifyContent: 'center',
+		display: 'flex',
+		alignItems: 'center',
+		flex: 1,
+	},
+
+	backgroundImage: {
+		width: '100%',
+		height: '100%',
+		justifyContent: 'flex-end',
+		alignItems: 'center',
+	},
+	cameraImage: {
+		width: 100,
+		height: 100,
+		marginBottom: 20,
+	},
+});
