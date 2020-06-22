daa035093dfbd1a41ceb90e8459135de96bbe9c0
Joakim <joakim@dojo.dk>
2020-06-22 09:30:53 +0200

    merges

--- App.tsx
+++ App.tsx
@@ -3,6 +3,9 @@ import AppNavigator from './AppNavigator';
 import { StatusBar } from 'react-native';
 import firebase from 'firebase';
 import { firebaseConfig } from './config';
+// import * as tf from '@tensorflow/tfjs';
+// import '@tensorflow/tfjs-react-native';
+// import { getModel } from './scripts/tensorTools';
 
 export default () => {
 	useEffect(() => {
@@ -11,6 +14,14 @@ export default () => {
 		if (firebase.auth().currentUser === null) {
 			firebase.auth().signInAnonymously();
 		}
+		// tf.ready()
+		// 	.then(() => {
+		// 		console.log('noice');
+		// 		// getModel()
+		// 		// 	.then(() => 'Got model')
+		// 		// 	.catch(e => console.error('model', e));
+		// 	})
+		// 	.catch(e => console.error('tensor', e));
 	}, []);
 
 	return <AppNavigator />;
